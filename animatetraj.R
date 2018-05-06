f9af55bc27d8430105239b372e4dd3a5801c5ce3
jevansbio <32939196+jevansbio@users.noreply.github.com>
2018-05-06 19:08:33 -0400

    Add files via upload

---
+++ animatetraj.R
@@ -0,0 +1,43 @@
+
+animatetraj=function(trajdf,basezoom){
+	require(ggmap)
+	for(i in 1:(nrow(trajdf)-1)){
+		if(trajdf$dist[i]>0){
+				zoom=round(zoom=basezoom-((trajdf$dist[i])))
+				if(zoom<2){
+					zoom=2
+				}
+			} else {
+				zoom=basezoom
+		}
+		if(i==1|trajdf$dist[i]>0){
+			gotmap=F
+			while(gotmap==F){
+			
+				g1=try(get_map(location=data.frame(trajdf$x[i],trajdf$y[i]),zoom=zoom,maptype = "satellite",messaging=F),T)
+				if(class(g1)[1]=="ggmap"){
+					m=g1
+					gotmap=T
+				} else {
+					#to avoid overloading google maps API if we fail we wait a bit
+					Sys.sleep(5)
+				}
+			}
+			mbb=as.numeric(attributes(m)$bb)
+		}
+		
+		if(i>1){
+			m2=ggmap(m,extent="normal")+
+			geom_path(data =trajdf[1:i,] , aes(x, y),colour="black",linetype = 2,show.legend=F)+
+			geom_point(data =trajdf[i,] , aes(x, y,colour="red",size=1.5),show.legend=F)+
+			coord_cartesian(xlim=mbb[c(2,4)],mbb[c(1,3)],expand=F)
+		} else {
+			m2=ggmap(m)+geom_point(data =trajdf[i,] , aes(x, y,colour="red",size=1.5),show.legend=F)
+
+		}
+	
+
+		plot(m2)
+	}
+}
+
